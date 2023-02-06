0965c4d9d9d63c354bfbd3637502375d8ed6fa5d
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-02-06 13:18:45 -0300

    fetching, deleting, error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map} from "rxjs/operators";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Post} from "./post.model";
 import {PostsService} from "./posts.service";
 import {Subscription} from "rxjs";
@@ -10,15 +9,25 @@ import {Subscription} from "rxjs";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching: boolean = false;
+  error: string = null;
+  private errorSubscription = new Subscription();
 
   constructor(private httpClient: HttpClient, private postsService: PostsService) {
   }
 
   ngOnInit() {
     this.onFetchPosts();
+    this.errorSubscription = this.postsService.error.subscribe((error) => {
+      this.error = error;
+      console.log(error);
+    });
+  }
+
+  ngOnDestroy() {
+    this.errorSubscription.unsubscribe();
   }
 
   onCreatePost(postData: Post) {
@@ -32,10 +41,21 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe((posts) => {
       this.loadedPosts = posts;
       this.isFetching = false;
+    }, error => { // the second argument of subscribe() is an error-handling function
+      this.error = error.statusText;
+      this.isFetching = false;
+      console.log(error);
     });
   }
 
   onClearPosts() {
     // Send Http request
+    this.postsService.clearPosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+
+  onHandleError() {
+    this.error = null;
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Post} from "./post.model";
-import {map} from "rxjs/operators";
+import {catchError, map} from "rxjs/operators";
+import {Subject, throwError} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  error: Subject<string> = new Subject<string>();
 
   constructor(private httpClient: HttpClient) {
   }
@@ -18,6 +20,8 @@ export class PostsService {
     this.httpClient.post<{ name: string }>('https://angular-http-requests-7afaf-default-rtdb.firebaseio.com/posts.json', postData) // angular will convert the postData to json automatically
       .subscribe(responseData => { // post will not work if there is no subscription to the resulting observable
         console.log(responseData); // responseData is the response's body extracted automatically
+      }, error => {
+        this.error.next(error.statusText);
       });
 
     // Posts send 2 requests, the first one is of type OPTIONS, to check if post method is allowed. If so,
@@ -27,19 +31,27 @@ export class PostsService {
   fetchPosts() {
     return this.httpClient.get<{ [key: string]: Post }>('https://angular-http-requests-7afaf-default-rtdb.firebaseio.com/posts.json') // this indicates that the response will be an object with a random string as a key, and it's type is Post
       .pipe(map((responseData) => { // the response data type is the one specified in the get() method
-        const postsArray: Post[] = []
+          const postsArray: Post[] = []
 
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            // postsArray.push(responseData[key]); << push the object direct to array
-            postsArray.push({id: key, ...responseData[key]}); // pushes an object with the key and every field of the response[key]
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              // postsArray.push(responseData[key]); << push the object direct to array
+              postsArray.push({id: key, ...responseData[key]}); // pushes an object with the key and every field of the response[key]
+            }
           }
-        }
 
-        return postsArray;
-      })); // the subscription will happen in the component
+          return postsArray;
+        }),
+        catchError(error => {
+          // handling errors here can be useful to perform generic operations, like sending data to analytics
+          return throwError(error); // return an observable created by throwError
+        })); // the subscription will happen in the component
     // .subscribe(posts => {
     //
     // });
+  }
+
+  clearPosts() {
+    return this.httpClient.delete('https://angular-http-requests-7afaf-default-rtdb.firebaseio.com/posts.json');
   }
 }
